fc5aa96c82ed70ef032d12b9d361a869bb3d4173
Caleb Sawtell <caleb.sawtell@gmail.com>
2020-04-11 23:33:18 +1200

    made server use env vars for db url

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,60 @@
+import * as dotenv from "dotenv"
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import * as PostgressConnectionStringParser from 'pg-connection-string';
+import {createConnection, ConnectionOptions, getConnectionOptions} from "typeorm";
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
 import * as bodyParser from 'koa-bodyparser';
 import { Drawing } from "./entity/Drawing";
 
+if (process.env.NODE_ENV !== 'production') {
+	dotenv.config()
+}
+
 const PORT = process.env.PORT || 3000;
-
-createConnection().then(async connection => {
-	const app = new Koa();
-	app.use(bodyParser());
-
-	const router = new Router();
-	router
-	.prefix("/api")
-	.post('/drawing', async (ctx) => {
-		console.log("new drawing", ctx.request);
-		const drawing = new Drawing();
-		drawing.version = ctx.request.body.version
-		drawing.canvas = ctx.request.body.canvas
-		await drawing.save()
-		ctx.body = drawing;
-	})
-	.patch('/drawing/:id', async (ctx) => {
-		const drawing = await Drawing.findOne({id:ctx.params.id})
-		drawing.canvas = ctx.request.body.canvas
-		await drawing.save()
-		ctx.body = drawing;
-	})
-	.get('/drawing/:id', async (ctx) => {
-		const drawing = await Drawing.findOne({id:ctx.params.id})
-		if(typeof drawing === "undefined")
-		{
-			ctx.status = 404
-		}else{
-			ctx.body = drawing;
+const connectionOptionsDBURL = PostgressConnectionStringParser.parse(process.env.DATABASE_URL);
+getConnectionOptions().then(
+	async connectionOption => {
+		try{
+			await createConnection(<ConnectionOptions>{ ...connectionOption, ...connectionOptionsDBURL})
 		}
-	});
+		catch(e){
+			console.log(e)
+		}
 
-	app.use(router.routes());
+		const app = new Koa();
+		app.use(bodyParser());
 
-	app.listen(PORT);
+		const router = new Router();
+		router
+			.prefix("/api")
+			.post('/drawing', async (ctx) => {
+				const drawing = new Drawing();
+				drawing.version = ctx.request.body.version
+				drawing.canvas = ctx.request.body.canvas
+				await drawing.save()
+				ctx.body = drawing;
+			})
+			.patch('/drawing/:id', async (ctx) => {
+				const drawing = await Drawing.findOne({ id: ctx.params.id })
+				drawing.canvas = ctx.request.body.canvas
+				await drawing.save()
+				ctx.body = drawing;
+			})
+			.get('/drawing/:id', async (ctx) => {
+				const drawing = await Drawing.findOne({ id: ctx.params.id })
+				if (typeof drawing === "undefined") {
+					ctx.status = 404
+				} else {
+					ctx.body = drawing;
+				}
+			});
 
-	console.log('Server running on port ' + PORT);
+		app.use(router.routes());
 
-}).catch(error => console.log(error));
+		app.listen(PORT);
+
+		console.log('Server running on port ' + PORT);
+
+	}
+)
